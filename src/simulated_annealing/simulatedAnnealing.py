fc0c535be4da13a9226b1d61406a7630fc28d9b9
larissa.fraga <larissa.fra345@gmail.com>
2021-06-27 13:52:43 -0400

    first commit

---
+++ src/simulated_annealing/simulatedAnnealing.py
@@ -0,0 +1,19 @@
+def main():
+    # arquivos de entrada
+    arquivos = 'arquivos/'
+    quantidadesSolucoes = int(input('Digite a quantidade de soluções por arquivo: '))
+
+    # arquivos de saida serao armazenados neste vetor
+    saida = []
+    
+    # percorrer os arquivos
+    #for arquivo in arquivos:    
+        #abre o arquivo 
+        #with open(arquivo) as f:
+            #linhas = f.readlines()
+            #saida = linhas
+            #print(saida)
+
+
+if __name__ == '__main__':
+   main()
